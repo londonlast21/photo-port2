1481f29c7ae94cc85bc51a7111ff41e06e021add
jack <biffisbuffm@gmail.com>
2020-10-09 10:38:48 -0500

    test plist

--- src/components/Gallery/__tests__/Photolist.test.js
+++ src/components/Gallery/__tests__/Photolist.test.js
@@ -2,3 +2,17 @@ import React from 'react'
 import { render, cleanup } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import PhotoList from '../PhotoList'
+import { renderIntoDocument } from 'react-dom/test-utils'
+
+afterEach(cleanup)
+
+describe('Photolist is rendering', () => {
+    it('renders', () => {
+        renderIntoDocument(<PhotoList />);
+    });
+
+    it('renders', () => {
+        const { asFragment } = render(<PhotoList />)
+        expect(asFragment()).toMatchSnapshot()
+    });
+});
